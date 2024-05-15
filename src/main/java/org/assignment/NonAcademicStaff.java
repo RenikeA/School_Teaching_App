4a128e48ba0e766c5fcf1b5b56b6bdf0782eaad1
Atinuke Akinboh <kiki@Atinukes-MacBook-Air.local>
2024-05-15 21:04:53 +0100

    initial commit

---
+++ src/main/java/org/assignment/NonAcademicStaff.java
@@ -0,0 +1,12 @@
+package org.assignment;
+
+public class NonAcademicStaff extends Staff {
+
+    public NonAcademicStaff() {
+    }
+
+    public NonAcademicStaff(String name, int age) {
+
+        super(name, age);
+    }
+}
